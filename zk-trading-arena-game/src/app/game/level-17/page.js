57c166ec4792733ac3d595387f762cfc1bc968bb
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2024-12-20 17:57:26 +0530

    level 16-18 added

---
+++ zk-trading-arena-game/src/app/game/level-17/page.js
@@ -0,0 +1,6 @@
+'use client';
+import { Level17 } from "../level1120";
+
+export default function Level11Page() {
+    return <Level17 />;
+}
---
+++ zk-trading-arena-game/src/app/game/level-18/page.js
@@ -0,0 +1,6 @@
+'use client';
+import { Level18 } from "../level1120";
+
+export default function Level11Page() {
+    return <Level18 />;
+}
