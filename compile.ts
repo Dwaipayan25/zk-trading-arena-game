734a4ea3205b3fd812f9bc7bed21c64f55596d01
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2024-12-25 22:49:46 +0530

    removed 2 files

--- compile.ts
+++
@@ -1,5 +0,0 @@
-import sindri from 'sindri';
-
-// Compile the circuit in the `profit-verifier` directory.
-const circuit = await sindri.createCircuit('profit-verifier');
-console.log(JSON.stringify(circuit, null, 2));
--- prove.ts
+++
@@ -1,8 +0,0 @@
-import sindri from 'sindri';
-
-const circuitIdentifier = 'profit-verifier:latest';
-const proofInput = 'initial_balance=1000\nfinal_balance=1150\nprofit_range=0';
-
-const proof = await sindri.proveCircuit(circuitIdentifier, proofInput);
-
-console.log(JSON.stringify(proof, null, 2));
