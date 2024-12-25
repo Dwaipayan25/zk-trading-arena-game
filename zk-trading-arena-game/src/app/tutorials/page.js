0a5df4a03282e5e1d36db50ffbd313d3ba780f46
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2024-12-25 23:11:10 +0530

    Starting to modify tutorials page

--- zk-trading-arena-game/src/app/tutorials/page.js
+++ zk-trading-arena-game/src/app/tutorials/page.js
@@ -1,10 +1,29 @@
+"use client";
+
+import React from "react";
+
 export default function Tutorials() {
-    return (
-      <div className="container">
-        <h1>Tutorials</h1>
-        <p>Learn trading and ZK-proof basics to dominate the game.</p>
-        {/* Add tutorial list or content here */}
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold mb-4">📚 Tutorials</h1>
+      <p className="text-lg mb-6">
+        Learn trading and ZK-proof basics to dominate the game!
+      </p>
+
+      <div className="my-8">
+        {/* Twitter Embed */}
+        <blockquote
+          className="twitter-tweet"
+          data-lang="en"
+          data-dnt="true"
+          data-theme="light"
+        >
+          <a href="https://x.com/zkTradingArena/status/1871941365086773394">
+            Tweet Link
+          </a>
+        </blockquote>
       </div>
-    );
-  }
-  
+      <h2>More coming soon!</h2>
+    </div>
+  );
+}
